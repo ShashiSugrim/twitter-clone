accf7babf4c1907b4781f3c8f3a86335d42ee2d4
salma o <salmaomran2@gmail.com>
2023-06-22 20:56:02 -0400

    signin page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import twitterLogo from './twitterlogo.png';
-
+import glass from './glass.png'
 import SignIn from './SignIn.js';
 
 function App() {
@@ -10,14 +10,15 @@ function App() {
 <div className="sidebar one">
 <img className="bird" alt="Twitter Logo" src={twitterLogo} />
  <br />
+    <img className = "glass" src = {glass} />
     <h2 className = "explore-side">Explore</h2>
 </div>
 <div className="sidebar two">
-    <h2>Explore</h2>
+    <h2 className = "explore-side2">Explore</h2>
 </div>
 <div className="sidebar three">
 <div className="new-to-twitter-box">
-      <h2>New to Twitter?</h2>
+      <h2 className = "new-to-text">New to Twitter?</h2>
       <p className = "sign-up-text"> Sign up now to get your own personalized timeline!</p>
       <button className="signup-button">Sign up</button>
     </div>
