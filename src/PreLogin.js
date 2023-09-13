279061fb43df10647c2d1622e1ff0a469591063e
Shashi Sugrim <ssugrim1430@bths.edu>
2023-09-13 07:33:31 -0400

    signup now redirects to seperate page

--- src/PreLogin.js
+++ src/PreLogin.js
@@ -4,9 +4,16 @@ import './prelogin.css';
 import twitterLogo from './twitterlogo.png';
 import glass from './glass.png';
 import SignIn from './SignIn.js';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import Explore from './Explore';
+import SignUp from './SignUp';
 
 function PreLogin() {
+  let navigate = useNavigate();
+  const handleSignUp = (event) =>
+  {
+    navigate("/signup")
+  }
   return (
 <div className="App">
 <div className="sidebar one">
@@ -16,13 +23,14 @@ function PreLogin() {
     <h2 className = "explore-side">Explore</h2>
 </div>
 <div className="sidebar two">
-    <h2 className = "explore-side2">Explore</h2>
+    {/* <h2 className = "explore-side2">Explore</h2> */}
+    <Explore className = "explore-side2"/>
 </div>
 <div className="sidebar three">
 <div className="new-to-twitter-box">
       <h2 className = "new-to-text">New to Twitter?</h2>
       <p className = "sign-up-text"> Sign up now to get your own personalized timeline!</p>
-      <button className="signup-button">Sign up</button>
+      <button onClick={handleSignUp} className="signup-button">Sign up</button>
       <Link to="/postlogin">Go to PostLogin</Link>
     </div>
 </div>
