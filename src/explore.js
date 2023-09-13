ceadd1dd4e4eb635d0d79a01cb1c2eac9693ffff
Shashi Sugrim <ssugrim1430@bths.edu>
2023-09-13 07:28:57 -0400

    added example tweets

---
+++ src/explore.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Tweet from './Tweet'
+
+const Explore = () => {
+  return (
+    <>
+        <h2>Explore</h2>
+        <Tweet></Tweet>
+        <hr/>
+        <Tweet></Tweet>
+        <hr/>
+        <Tweet></Tweet>
+        <hr/>
+    </>
+  )
+}
+
+export default Explore
