95cc7915328bfe726043daadb2fd79a692852b58
Shashi Sugrim <ssugrim1430@bths.edu>
2023-09-15 11:22:12 -0400

    calling request to server to get tweets

--- src/explore.js
+++ src/explore.js
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Tweet from './Tweet'
+import axios from "axios";
 
 const Explore = () => {
+  const[tweets, setTweets] = useState([]);
+
+  async function getTweets() {
+    const results = await axios.get("http://localhost:3001/tweets");
+    console.log("Results are " + JSON.stringify(results));
+    setTweets(results.data);
+  }
+
+  useEffect(()=>{
+    let ignore = false;
+
+    if (!ignore) getTweets();
+
+    return () => {
+      ignore = true;
+    };
+  }
+    , []
+  )
+
   return (
     <>
         <h2>Explore</h2>
-        <Tweet></Tweet>
         <hr/>
-        <Tweet></Tweet>
-        <hr/>
-        <Tweet></Tweet>
-        <hr/>
+        {tweets? tweets.map((item, index)=>{ return (<><Tweet username={item.username} title={item.title} description={item.description} time_stamp={item.date_created}/><hr/></>)}): null}
+
     </>
   )
 }
